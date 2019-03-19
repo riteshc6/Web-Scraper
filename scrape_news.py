4ef6483522baa96482c6738a83343bef821f262b
Ritesh Chaurasia <riteshc6@gmail.com>
2019-03-19 11:13:49 +0530

    Included upvotes and author names to capture in scraper

--- scrape_news.py
+++ scrape_news.py
@@ -7,17 +7,31 @@ news_html = requests.get("https://news.ycombinator.com/news").text
 
 news_csv = open("news.csv","w")
 csv_writer = csv.writer(news_csv)
-csv_writer.writerow(["Title", "Link"])
+csv_writer.writerow(["Title", "Link", "Upvotes","Author"])
 
 news = BeautifulSoup(news_html, "lxml")
+#body = news.find("td", class_ = "subtext")
 
-#head = news.find('tr', class_ = "athing")
-for title in news.find_all(class_="athing"):
+
+# head = news.find('tr', class_ = "athing")
+for title, sub in zip(news.find_all(class_="athing"),news.find_all(class_ = "subtext")):
+    
     headline = title.find(class_="storylink").text
-    link = title.find(class_="storylink")['href']
-    print(headline)
-    print(link)
-    print()
-    csv_writer.writerow([headline, link])
+    print(headline)    
+    
+    try :    
+        link = title.find(class_="storylink")['href']
+        upvotes = sub.find("span", class_ = "score").text
+        author = sub.find(class_ = "hnuser").text
+        print(link)
+        print(upvotes, author)
+        print()
+        csv_writer.writerow([headline, link, upvotes, author])
+
+    except Exception as e:
+        author = None
+        upvotes = None
+        print("HTML page broken")
+            
 
 news_csv.close()
